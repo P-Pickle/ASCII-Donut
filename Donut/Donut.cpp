a3ad597db992321583b046c8c502a372d643923d
P-Pickle <dragonslayer62601@gmail.com>
2023-01-17 22:55:52 -0600

    Linked the size of zbuffer to the screenwidth and screenheight so the setting are easier changed, also changed the K1 equation to push the donut farther back

--- Donut/Donut.cpp
+++ Donut/Donut.cpp
@@ -37,30 +37,56 @@ void ResetOutput(char** output, float screenwidth, float screenheight)
 	}
 }
 
+float** CreateBuffer(float screenwidth, float screenheight)
+{
+	float** Zbuffer = new float*[screenwidth];
+
+	for (int x = 0; x < screenwidth; x++)
+	{
+		Zbuffer[x] = new float[screenheight]();
+	}
+
+	
+	return Zbuffer;
+}
+
+void ResetBuffer(float** zbuffer, float screenwidth, float screenheight)
+{
+
+	for (int y = 0; y < screenheight; y++)
+	{
+		for (int x = 0; x < screenwidth; x++)
+		{
+			zbuffer[x][y] = 0;
+		}
+	}
+}
+
+
 void Donut(float A, float B)
 {
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 	COORD pos = { 0, 0 };
 
-	const float screenwidth = 50;
+	const float screenwidth = 60;
 	const float screenheight = 25;
-	const float theta_spacing = .07;
+	const float theta_spacing = 0.07;
 	const float phi_spacing = .02;
 
 	const float R1 = 1;
 	const float R2 = 2;
-	const float K2 = 15;
+	const float K2 = 5;
 
-	const float K1 = 65/*screenwidth * K2 * 3 / (8 * (R1 + R2))*/;
+	const float K1 = /*65*/(screenwidth * K2 * 3 / (8 * (R1 + R2)))/2;
 
 	char** output = CreateOutput(screenwidth, screenheight);
-	
+	float** zbuffer = CreateBuffer(screenwidth, screenheight);
 	
 
 	while (true)//this is the loop that renders all the frames
 	{
 		ResetOutput(output, screenwidth, screenheight);
-		float zbuffer[50][25] = { 0 };
+		ResetBuffer(zbuffer, screenwidth, screenheight);
 
 		float CosA = cos(A), SinA = sin(A);
 		float CosB = cos(B), SinB = sin(B);
@@ -122,11 +148,10 @@ void Donut(float A, float B)
 }
 
 
-
-
 void main()
 {
 	float A = 0, B = 0;
+
 
 	Donut(A, B);
 
